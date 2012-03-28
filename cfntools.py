b4eb5eba6d4ac6010548f611c46d831e58a00813
Johann Visagie <johann@visagie.za.net>
2012-03-28 13:30:54 +0200

    Point checking. Not necessarily working.

--- cfntools.py
+++ cfntools.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
+import types
+
+from rdict import rdict
+
 import json
 from bidict import bidict
 import cloudformation as cfn
 
 def fn_join(sep, lst):
   return {'Fn:Join': [sep, lst]}
+
+# XXX We need a class that can encapsulate a string that contains references
 
 class Stack(object):
 
@@ -84,14 +90,18 @@ class Mapping:
 
 class Resource(object):
 
-  keymap = {'Properties': 'properties', 'Metadata': 'metadata'}
+  keymap = {'Type': 'resource_type',
+            'Properties': 'properties',
+            'Metadata': 'metadata',
+            'DependsOn', 'depends_on',
+            'DeletionPolicy', 'deletion_policy'}
 
   def __init__(self, resource_type):
     self.resource_type = resource_type
 
   @property
   def template(self):
-    T = {'Type': self.resource_type}
+    T = {}
     for key, attrib_name in self.keymap.items():
       try:
         attrib = getattr(self, attrib_name)
@@ -103,6 +113,77 @@ class Resource(object):
     return T
 
 
+class CFNInit(Resource):
+
+  from string import ascii_lowercase as a_l
+  cmd_indices = [c + d for c in a_l for d in a_l]
+  # XXX must delete these two unless we can come up with a rdict we like
+  config_keys = ['commands', 'files', 'groups', 'packages', 'services',
+                 'sources', 'users']
+  pkg_types = ['yum', 'python']                 # XXX expand
+
+  # Is officialy a Resource Type, but doesn't quite act like one --
+  # no Type attribute, no Properties, &c.
+
+  def __init__(self, configsets={}, configs={'config': {}}):
+    self.configsets = {}
+    self.configs = configs
+
+  # XXX If there aren't multiple configs, then we operate on one config named
+  # 'config' -- how best to implement?
+
+  def add_config(self, name):
+    self.configs[name] = {}
+
+  def add_package(self, config, pkg_type, name, versions=[]):
+    if not isinstance(versions, list):
+      versions = [versions]
+    if not 'packages' in config:
+      config['packages'] = {}
+    if not 'pkg_type' in config['packages']:
+      config['packages'][pkg_type] = {}
+    config['packages'][pkg_type][name] = versions
+
+  def _next_cmdprefix(self, prefix):
+    return self.cmd_indices[self.cmd_indices.index(prefix) + 1]
+
+  def add_command(self, config, name, command):
+    """Add a command to config.
+
+    Commands are executed in alphabetical order. Hence, we prefix command names
+    with "aa_", "ab_", ...
+
+    """
+    # TODO add support for env, cwd, test, ignoreErrors
+    # XXX Figure out a way that commands can have references
+    assert isinstance(command, (str, list)), \
+        "%s: command must be a string or list" % self.__name__
+    if not 'commands' in config:
+      config['commands'] = {}
+      prefix = 'aa'
+    else:
+      last_prefix = sorted(config['commands'].keys())[-1].split('_')[0]
+      prefix = self._next_cmdprefix(last_prefix)
+    config['commands']['_'.join(prefix, name)] = command
+
+  # configsets is a dict of lists
+  def add_configset(self, name, configlist):
+    self.configsets[name] = []
+    for config in configlist:
+      if config not in self.configs:
+        self.add_config(config)
+      self.configsets[name].append(config)
+
+  @property
+  def template(self):
+    D = {}
+    if self.configsets:
+      pass # D['configsets'] = 
+    for config in self.configs:
+      pass # D['config'] = 
+    return {'AWS::CloudFormation::Init': D}
+    
+
 class BaseInstance(Resource):
 
   def __init__(self):
@@ -113,17 +194,27 @@ class BaseInstance(Resource):
 
 class EC2Instance(BaseInstance):
 
-  def __init__(self, image_id, key, instance_type, userdata_files=[],
+  def __init__(self, image_id, keypair, instance_type, userdata_files=[],
                userdata_script='', security_groups=[], tags=[]):
+
     super(EC2Instance, self).__init__()
+
     self.properties['InstanceType'] = instance_type
     self.properties['ImageId'] = image_id
-    self.properties['KeyName'] = key
-    self.properties['SecurityGroups'] = security_groups
-    self.properties['Tags'] = tags
+    self.properties['KeyPair'] = keypair
+
+    if security_groups:
+      self.properties['SecurityGroups'] = security_groups
+    else:
+      self.properties['SecurityGroups'] = ['default']
+
+    if tags:
+      self.properties['Tags'] = tags
+
+    # userdata_files takes precedence over userdata_script
     if userdata_files:
       self.properties['UserData'] = self._format_userdata(userdata_files)
-    if userdata_script:
+    elif userdata_script:
       self.properties['UserData'] = fn_join('', userdata_script.readliens())
 
     self.cfn_config = {}
