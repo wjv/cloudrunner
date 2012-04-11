cda18b48921835f8884ec24d2e7bd7a350ca9510
Johann Visagie <johann@visagie.za.net>
2012-04-11 23:47:23 +0200

    Add content to ResourcePropertyTypes; not sure why this didn't go in prev. rev.

--- CloudRunner/ResourcePropertyTypes.py
+++ CloudRunner/ResourcePropertyTypes.py
@@ -1,1 +1,24 @@
 """AWS Resource Property Types"""
+
+
+class ResourceProperty(object):
+
+  @property
+  def template(self):
+    return self.properties
+
+
+class EC2SecurityGroupRule(ResourceProperty):
+
+  # XXX anything in stdlib for handling IP addresses &c.?
+
+  def __init__(self, ip_protocol, from_port, to_port, cidr_ip='',
+               sourcegroup=''):
+    self.properties = {}
+    self.properties['IpProtocol'] = ip_protocol
+    self.properties['FromPort'] = from_port
+    self.properties['ToPort'] = to_port
+    if sourcegroup:
+      self.properties['SourceSecurityGroupName'] = sourcegroup
+    else:
+      self.properties['CidrIp'] = cidr_ip
