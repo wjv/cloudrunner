20671dcd36e42d01e847779f55737201fdab24fe
Johann Visagie <johann@visagie.za.net>
2012-04-12 10:13:10 +0200

    Start adapting for new regime

--- example.py
+++ example.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-import json
 from StringIO import StringIO
 
-from cfntools import *
+from CloudRunner.ResourceTypes import *
+from CloudRunner.CFN import Stack
 
 myscript = StringIO("""#!/bin/bash
 
